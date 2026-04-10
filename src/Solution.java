3cbc37f6b633e6fdfc2527bd405cd48a323f68f8
vaishnav <vaishnssavkunschuaa@gmail.com>
2026-04-10 22:06:34 +0530

    Solution

--- src/Solution.java
+++ src/Solution.java
@@ -1,7 +1,4 @@
-
-
-
-import java.util.HashMap;
+import java.util.TreeMap;
 import java.util.Map;
 import java.util.Scanner;
 
@@ -9,25 +6,19 @@ public class Solution {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         
-        // Use a HashMap to store word frequencies
-        HashMap<String, Integer> frequencyMap = new HashMap<>();
+        // Use a TreeMap to automatically sort keys alphabetically
+        TreeMap<String, Integer> frequencyMap = new TreeMap<>();
 
-        // Read the line of text
         if (sc.hasNextLine()) {
             String input = sc.nextLine();
-            
-            // Split the text into individual words by spaces
             String[] words = input.split("\\s+");
 
             for (String word : words) {
-                // If word is empty (can happen with extra spaces), skip it
                 if (word.isEmpty()) continue;
-                
-                // Update the frequency in the map
                 frequencyMap.put(word, frequencyMap.getOrDefault(word, 0) + 1);
             }
 
-            // Print the unique words and their counts
+            // Printing from a TreeMap will now follow alphabetical order
             for (Map.Entry<String, Integer> entry : frequencyMap.entrySet()) {
                 System.out.println(entry.getKey() + ": " + entry.getValue());
             }
